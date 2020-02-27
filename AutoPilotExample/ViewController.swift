e308b473da6acee130ed6b8c092b4900a98ab4b8
Ella <s013418@students.lmsd.org>
2020-02-26 22:27:47 -0500

    adding stuff

--- AutoPilotExample/ViewController.swift
+++ AutoPilotExample/ViewController.swift
@@ -7,24 +7,23 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let _ = Auth.auth().currentUser{
+            self.performSegue(withIdentifier: "toHomeScreen", sender: self)
+        }
+        
     }
-    */
 
 }
