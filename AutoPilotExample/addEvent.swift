db4976cf395d80253ba1abb5ded71b124db94e86
Emily Shang <s014294@students.lmsd.org>
2020-02-27 10:14:43 -0500

    Created new view controller for add an event, and created the file to go with it.

---
+++ AutoPilotExample/addEvent.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AutoPilotExample
+//
+//  Created by Ella Lawrence (student LM) on 2/26/20.
+//  Copyright © 2020 Ella Lawrence (student LM). All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class addEvent: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        if let _ = Auth.auth().currentUser{
+//            self.performSegue(withIdentifier: "toHomeScreen", sender: self)
+//        }
+        
+    }
+
+}
