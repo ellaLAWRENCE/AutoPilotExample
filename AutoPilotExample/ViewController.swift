dff2b8e1aac5a827813f6c466e2136d215eb8561
Noah Scher <neway1005@icloud.com>
2020-03-02 07:53:37 -0500

    voice attempt #2

--- AutoPilotExample/ViewController.swift
+++ AutoPilotExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("voice")
+        print("hello")
     }
 
     override func viewDidAppear(_ animated: Bool) {
