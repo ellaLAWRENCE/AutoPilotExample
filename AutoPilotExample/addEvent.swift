4bf288c18b9494f71eaad73891c663718a9f66bb
Ella <s013418@students.lmsd.org>
2020-03-31 11:51:16 -0400

    added stuff

--- AutoPilotExample/addEvent.swift
+++ AutoPilotExample/addEvent.swift
@@ -56,7 +56,48 @@ class addEvent: UIViewController, UITextFieldDelegate {
         return true
     }
     
+        
+        
+        @IBAction func addNow(_ sender: UIButton) {
+
+            let eventStore = EKEventStore()
+            
+            let startDate = NSDate()
+            let endDate = startDate.addingTimeInterval(60 * 60) // One hour
+            
+            if (EKEventStore.authorizationStatus(for: .event) != EKAuthorizationStatus.authorized) {
+                eventStore.requestAccess(to: .event, completion: {
+                    granted, error in
+                    self.createEvent(eventStore: eventStore, title: "Test Event", startDate: startDate, endDate: endDate)
+                })
+            } else {
+                createEvent(eventStore: eventStore, title: "Test Event", startDate: startDate, endDate: endDate)
+            }
+            
+        }
+        
+        func createEvent(eventStore: EKEventStore, title: String, startDate: NSDate, endDate: NSDate) {
+            let event = EKEvent(eventStore: eventStore)
+            
+            event.title = assignmentNameText.text
+            event.startDate = startDate as Date
+            event.endDate = endDate as Date
+            event.calendar = eventStore.defaultCalendarForNewEvents
+            do {
+                try eventStore.save(event, span: .thisEvent)
+                savedEventId = event.eventIdentifier
+            } catch {
+                print("Bad things happened")
+            }
+        }
+        
+
+            var timeLeft = [Int]()
+            let dateFormatter = DateFormatter()
+            
+            
+}
+
+
     
 
-
-}
